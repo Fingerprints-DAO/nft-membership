fb8d4f0e447234bc60ea9b3abe9d5c8fe04eeb9a
Thiago Lobão <thiago@blank.st>
2023-07-31 22:44:35 -0300

    [Auction] Finish first version

--- packages/dapp-web-app/src/app/(institutional)/auction/nft-card.tsx
+++ packages/dapp-web-app/src/app/(institutional)/auction/nft-card.tsx
@@ -5,11 +5,11 @@ import Image from 'next/image'
 
 const NftCard = () => {
   return (
-    <Card bg="gray.900" mb={[20, 20, 10, 0]} boxShadow="md">
-      <CardBody px={4} pt={6} pb={8}>
-        <AspectRatio maxW="full" w="auto" h="auto" ratio={4 / 3.5} borderTopRadius={8} overflow="hidden">
+    <Card bg="gray.900" mb={[20, 0]} boxShadow="md">
+      <CardBody px={{ base: 4, md: 8 }} pt={6} pb={8}>
+        <AspectRatio maxW="full" w="auto" h="auto" ratio={{ base: 4 / 3.5, md: 4 / 3.5 }} borderTopRadius={8} overflow="hidden">
           <Link href="#" target="_blank">
-            <Image alt="Fingerprints Membership #1" src={require(`/public/images/nft-card.png`)} priority={true} width={368} height={368} />
+            <Image alt="Fingerprints Membership #1" src={require(`/public/images/nft-card.png`)} priority={true} width={373} height={340} />
           </Link>
         </AspectRatio>
         <Box bg="gray.800" borderBottomRadius={8} p={4} mb={10}>
@@ -37,7 +37,7 @@ const NftCard = () => {
           <Text fontSize="md" color="gray.400" mb={2}>
             Auction starts in
           </Text>
-          <Text fontSize="2xl" fontWeight="bold" color="gray.100">
+          <Text fontSize={{ base: '2xl', md: '3xl' }} fontWeight="bold" color="gray.100">
             2d 1h 13min
           </Text>
         </Box>
@@ -46,7 +46,7 @@ const NftCard = () => {
             <Text fontSize="md" color="gray.400" mb={2}>
               Initial price
             </Text>
-            <Text fontSize="xl" fontWeight="bold" color="gray.100">
+            <Text fontSize={{ base: 'xl', md: '2xl' }} fontWeight="bold" color="gray.100">
               0.5 ETH
             </Text>
           </Box>
@@ -54,7 +54,7 @@ const NftCard = () => {
             <Text fontSize="md" color="gray.400" mb={2}>
               Duration
             </Text>
-            <Text fontSize="xl" fontWeight="bold" color="gray.100">
+            <Text fontSize={{ base: 'xl', md: '2xl' }} fontWeight="bold" color="gray.100">
               1 hour
             </Text>
           </Box>
