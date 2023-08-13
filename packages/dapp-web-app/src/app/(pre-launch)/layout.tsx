872fe63de4f37983dd968b1376026a297bb2d173
Arod <arod.mail+fp@proton.me>
2023-08-13 18:31:12 -0300

    fix(home): size on mobile

--- packages/dapp-web-app/src/app/(pre-launch)/layout.tsx
+++ packages/dapp-web-app/src/app/(pre-launch)/layout.tsx
@@ -1,37 +1,30 @@
 'use client'
 
-import { PropsWithChildren } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
-import { Box } from '@chakra-ui/react';
+import { PropsWithChildren } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+import { Box } from '@chakra-ui/react'
 
 const RootLayout = ({ children }: PropsWithChildren) => {
-  const bgImage = '/images/voxelgif-opt.gif';
+  const bgImage = '/images/voxelgif-opt.gif'
 
   return (
     <AnimatePresence>
       <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
         <Box
           as="section"
-          minH="100vh"
+          minH="full"
           minW="100vw"
           bg={`url(${bgImage}) center/cover no-repeat`}
           pos="relative"
         >
-          <Box
-            w="full"
-            minH="100%"
-            bg="black"
-            opacity={0.7}
-            pos="absolute"
-            zIndex={0}
-          />
+          <Box w="full" minH="full" bg="black" opacity={0.7} pos="absolute" zIndex={0} />
           <Box zIndex={1} pos="relative">
             {children}
           </Box>
         </Box>
       </motion.div>
     </AnimatePresence>
-  );
-};
+  )
+}
 
-export default RootLayout;
+export default RootLayout
